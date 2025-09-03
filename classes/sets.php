1aa576d32800726f1993521009495bc3c02e114a
Rick <johndoe@example.com>
2025-09-03 11:03:29 +0200

    hoi

--- classes/sets.php
+++ classes/sets.php
@@ -17,6 +17,45 @@ class Sets
 
 
 
+    public static function setsLatenZienInDeIndex()
+    {
+        
+        $conn = Database::start();
+
+        // voorbeeld
+        $query = "SELECT * FROM sets";
+        $resultaat = $conn->query($query);
+
+        $setss =[];
+
+        if ($resultaat->num_rows > 0) 
+        {
+            while ($row = $resultaat->fetch_assoc()) 
+            {
+                $set = new Sets();
+
+                $set->setId = $row['set_id'];
+                $set->setName = $row['set_name'];
+                $set->setDescription = $row['set_description'];
+                $set->setBrandId = $row['set_brand_id'];
+               
+
+
+
+
+                $setss[] = $set;
+            }
+            $conn->close(); // sluit de verbinding
+
+            return $setss;
+        }
+
+
+
+
+    }
+
+
 
     
     
